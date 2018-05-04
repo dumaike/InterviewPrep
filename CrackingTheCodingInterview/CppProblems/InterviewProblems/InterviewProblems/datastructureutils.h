785d3df3f664eb743868fbdb53e536ca7a98e598
dumaike <mdwyer84@gmail.com>
2018-05-04 17:12:11 -0400

    Some comment clarity and consistence in utils

--- CrackingTheCodingInterview/CppProblems/InterviewProblems/InterviewProblems/datastructureutils.h
+++ CrackingTheCodingInterview/CppProblems/InterviewProblems/InterviewProblems/datastructureutils.h
@@ -22,10 +22,10 @@ namespace com {
 
     public:
 
-      /* Creates and returns this graph
-      0 -> 1 -> 2
-      0 -> 3 -> 4 -> 5
-      4 -> 1
+      /* Creates and returns this graph (DOT notation)
+      / 0 -> 1 -> 2
+      / 0 -> 3 -> 4 -> 5
+      / 4 -> 1
       */
       static vector<com::graph::Node> createGraph1();
 
@@ -61,20 +61,20 @@ namespace com {
 
       /*
       Creates and returns this binary tree
-                  0
-                 /
-                1
-               /
-              2
-             /
-            3
-           /
-          4
+      /            0
+      /           /
+      /          1
+      /         /
+      /        2
+      /       /
+      /      3
+      /     /
+      /    4
       */
       static com::binarytree::Node* createTree3();
 
       /*
-      Creates and returns a binary tree with just a single root node
+      / Creates and returns a binary tree with just a single root node
       */
       static com::binarytree::Node* createTree4();
 
